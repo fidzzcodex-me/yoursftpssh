6b8e53d9af39376398a4d2d54aab067629ce7aca
fidzzcodex.me <rinatonidi28@gmail.com>
2026-06-19 15:29:56 +0700

    upload: app/src/main/java/com/fidzzcodex/sshftp/MainActivity.kt

---
+++ app/src/main/java/com/fidzzcodex/sshftp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.fidzzcodex.sshftp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.fidzzcodex.sshftp.ui.theme.SSHFTPTheme
+import com.fidzzcodex.sshftp.ui.navigation.AppNavigation
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            SSHFTPTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = SSHFTPTheme.colors.background
+                ) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
